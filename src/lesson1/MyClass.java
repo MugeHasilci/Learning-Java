e90352d28fb0b8224c32ee6b0979d25b08eb7c19
muge <muge.hasilci@hotmail.com>
2019-03-14 22:52:34 +0200

    getting user input

--- src/lesson1/MyClass.java
+++ src/lesson1/MyClass.java
@@ -1,10 +1,17 @@
 package lesson1;
 
+import java.util.Scanner;
+
 public class MyClass {
 
 	public static void main(String[] args) {
 		// TODO Auto-generated method stub
-		System.out.println("Hello Youtube");
+		Scanner scan = new Scanner (System.in);
+		System.out.println("Enter some number");
+		int user_input_number = scan.nextInt();
+		
+		System.out.println("The entered value is");
+		System.out.print(user_input_number);
 
 	}
 
