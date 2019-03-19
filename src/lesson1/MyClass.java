fa3d17b7de418b6c8100d1b4dd26fc976e19f94e
muge <muge.hasilci@hotmail.com>
2019-03-19 12:34:09 +0000

    incerement operator

--- src/lesson1/MyClass.java
+++ src/lesson1/MyClass.java
@@ -6,7 +6,7 @@ public class MyClass {
 
 	public static void main(String[] args) {
 		// TODO Auto-generated method stub
-		Scanner scan = new Scanner (System.in);
+		/*Scanner scan = new Scanner (System.in);
 		System.out.println("Enter some number");
 		int user_input_number = scan.nextInt();
 		
@@ -15,18 +15,22 @@ public class MyClass {
 		
 		Scanner scan1 = new Scanner (System.in);
 		System.out.println("Enter some decimal number");
-		double user_input_number_double = scan1.nextDouble();
+		double user_input_number_double = scan.nextDouble();
 		
 		System.out.println("The entered value is");
 		System.out.print(user_input_number_double);
 		
 		Scanner scan2 = new Scanner (System.in);
 		System.out.println("Enter some text");
-		String user_input_string = scan2.nextLine();
+		String user_input_string = scan.nextLine();
 		
 		System.out.println("The entered value is");
 		System.out.print(user_input_string);
- 
+		*/
+		
+		int x = 11;
+		x++;
+		System.out.println(x);
 	}
 
 }
