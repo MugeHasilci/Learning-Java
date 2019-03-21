e187e0b696ea507159d8264475eb21206b08d8aa
muge <muge.hasilci@hotmail.com>
2019-03-21 07:56:43 +0000

    logical operators

--- src/lesson1/MyClass.java
+++ src/lesson1/MyClass.java
@@ -26,22 +26,32 @@ public class MyClass {
 		
 		System.out.println("The entered value is");
 		System.out.print(user_input_string);
-		*/
 		
-		/* int x = 11;
+		
+		int x = 11;
 		x += 5;
 		System.out.println(++x);
 		System.out.println(x);
-		*/
+		
 		
 		int x = 10;
 		
 		if (x != 20) {
 			System.out.println("Yes x == 10");
-		}
+		}	
 		else {
 			System.out.println("no x != 10");
+		}*/
+		
+		int subject1 = 35;
+		int subject2 = 45;
+		
+		if(subject1 >= 30 && subject2 >= 45) {
+			System.out.print("The condition is true");
+		} else {
+			System.out.print("The condition is wrong");
 		}
-	}
+	} 
+	
 
 }
